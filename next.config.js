557c3e3de25b519d85f7fcd0ad64f3c3e0e77a6b
Elliott Chong <elliottchong16@gmail.com>
2023-09-19 09:17:19 +0800

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
