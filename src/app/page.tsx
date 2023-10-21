2fc39df5867414c14043da5308cb2a72f2d4568b
Nicola Migone <82510639+nicolello-dev@users.noreply.github.com>
2023-10-21 09:44:43 -0700

    Update two typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,8 +45,8 @@ export default async function Home() {
           </div>
 
           <p className="max-w-xl mt-1 text-lg text-slate-600">
-            Join millions of students, researchers and professinals to instantly
-            anwer questions and understand research with AI
+            Join millions of students, researchers and professionals to instantly
+            answer questions and understand research with AI
           </p>
 
           <div className="w-full mt-4">
